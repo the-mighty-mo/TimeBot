75598bb66e62fa95d0a7ef152d50d206d3428f1f
Benjamin Hall <hallb1016@gmail.com>
2020-10-18 01:43:50 -0400

    Added a help command

---
+++ TimeBot/Modules/Help.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace TimeBot.Modules
+{
+    public class Help : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        public async Task HelpAsync()
+        {
+            EmbedBuilder embed = new EmbedBuilder()
+                .WithColor(SecurityInfo.botColor)
+                .WithTitle(SecurityInfo.botName)
+                .WithCurrentTimestamp();
+
+            EmbedFieldBuilder prefix = new EmbedFieldBuilder()
+                .WithIsInline(false)
+                .WithName("Prefix")
+                .WithValue("\\" +
+                    "\n**or**\n" +
+                    Context.Client.CurrentUser.Mention + "\n\u200b");
+            embed.AddField(prefix);
+
+            EmbedFieldBuilder field = new EmbedFieldBuilder()
+                .WithIsInline(false)
+                .WithName("Commands")
+                .WithValue(
+                    "setchannel [channel mention/channel ID]\n" +
+                    "  - Sets the time channel"
+                );
+            embed.AddField(field);
+
+            await Context.Channel.SendMessageAsync(embed: embed.Build());
+        }
+    }
+}
