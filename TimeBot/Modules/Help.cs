6a87a15b027e9d064bab87e6cbf1fcc2b0a668d5
Benjamin Hall <hallb1016@gmail.com>
2020-10-21 01:51:28 -0400

    Reverted changing the title of help

--- TimeBot/Modules/Help.cs
+++ TimeBot/Modules/Help.cs
@@ -11,7 +11,7 @@ namespace TimeBot.Modules
         {
             EmbedBuilder embed = new EmbedBuilder()
                 .WithColor(SecurityInfo.botColor)
-                .WithTitle("Help");
+                .WithTitle(SecurityInfo.botName);
 
             EmbedFieldBuilder prefix = new EmbedFieldBuilder()
                 .WithIsInline(false)
